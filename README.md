1e2f8a790f86345a380da64d9cbf23d387d72f1c
liuyi03 <liuyi03@tal.com>
2023-02-16 18:08:04 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vue-quill-editor-image-plugin
-vue-quill-editor-image-plugin
+
+vue-quill-editor 图片处理插件
---
+++ index.js
@@ -0,0 +1,11 @@
+// vue-quill-editor 图片上传处理插件
+export class QuillImagePlugin {
+/**
+ * 构造函数
+ * @param {*} quill quill-editor实例
+ * @param {*} options 插件配置
+ */
+  constructor(quill, options) {
+    // TODO: 配置设置
+  }
+}
